4d5e37b31709e11ed94e34b7f4723f856b85527a
usama usman <69691249+usamadae@users.noreply.github.com>
2025-04-11 14:48:46 +0500

    fsd

--- src/app/utils/api/category.ts
+++ src/app/utils/api/category.ts
@@ -3,22 +3,12 @@ import axiosInstance from "../../lib/axios";
 export async function getTopCategories() {
   try {
     const res = await axiosInstance.get("/api/Category/Get4Bestcategories");
-
     return res.data;
   } catch (err) {
-    throw new Error("Failed to get top categories");
+    console.error('Error fetching top categories, using fallback:', err);
+    return [];  // Return fallback data (empty array or some default value)
   }
 }
 
-export async function getAllCategories() {
-  const res = await axiosInstance.get("/api/Category/GetAllCategory");
-  return res.data;
-  
-}
 
 
-export async function GetAllSubCategoriesWithCategory() {
-    const res = await axiosInstance.get("/api/Category/GetAllSubCategoriesWithCategory");
-    return res.data;
-    
-  }
