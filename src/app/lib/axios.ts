95c0cdaeacc4e7426dfa35032d21d874cd96ba06
sarmad522 <sarmadoctawave984@gmail.com>
2025-06-03 14:36:12 +0500

    updated code

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,17 +1,16 @@
-// src/lib/axios.ts
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
-// No token handling or auth logic
 axiosInstance.interceptors.request.use(
   (config) => {
-    // No token setting
     return config;
   },
   (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
+
+
